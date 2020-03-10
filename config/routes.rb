b7cc5fbcc4c1d52080e9616c481bd0ea399bd61c
Lee Jeong Min <danmin20@khu.ac.kr>
2020-03-10 16:06:50 +0900

    create & read

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # enter the address and connect to index action on the home controller
+  get '/index' => 'home#index'
+  get '/write' => 'home#write'
+  post '/create' => 'home#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
